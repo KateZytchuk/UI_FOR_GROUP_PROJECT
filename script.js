07903f5d8971cdd9b48640a6b7a1ceab36f72e9d
zaets39 <katezytchuk24@gmail.com>
2019-12-10 16:23:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var c =12;
+
+let a = '33';
+const bb ='some';
+
+console.log(`For manual debugging`);
+
+//примитивные типы данных в js
+console.log(typeof 11); //выводит на вывод тип значения
+console.log(typeof "hello");
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof {}); //это объект!!!
+
+const o = {};
+o.name = "Ivan"; //таких свойств может быть вагон
+console.log(o);
+
+console.dir(o)//вывод директивы
+
