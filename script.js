5bfe9497f6d3bbeee339d6bfab3fca5dfd281fe8
zaets39 <katezytchuk24@gmail.com>
2019-12-10 18:36:02 +0200

    second commit

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-var c =12;
-
-let a = '33';
-const bb ='some';
-
-console.log(`For manual debugging`);
-
-//примитивные типы данных в js
-console.log(typeof 11); //выводит на вывод тип значения
-console.log(typeof "hello");
-console.log(typeof true);
-console.log(typeof null);
-console.log(typeof undefined);
-console.log(typeof {}); //это объект!!!
-
-const o = {};
-o.name = "Ivan"; //таких свойств может быть вагон
-console.log(o);
-
-console.dir(o)//вывод директивы
-
+function isOk(){
+    alert("Button was clicked! :)")
+}
+function EconomClick(){
+    document.all.econom.src = "/cars/EconomCar.png";
+    document.all.business.src = "/cars/BusinessCarBW.png";
+    document.all.VIP.src = "/cars/VIPCarBW.png";
+    document.all.minivan.src = "/cars/MinivanCarBW.png";
+}
+function businessClick(){
+    document.all.econom.src = "/cars/EconomCarBW.png";
+    document.all.business.src = "/cars/BusinessCar.png";
+    document.all.VIP.src = "/cars/VIPCarBW.png";
+    document.all.minivan.src = "/cars/MinivanCarBW.png";
+}
+function VIPClick(){
+    document.all.econom.src = "/cars/EconomCarBW.png";
+    document.all.business.src = "/cars/BusinessCarBW.png";
+    document.all.VIP.src = "/cars/VIPCar.png";
+    document.all.minivan.src = "/cars/MinivanCarBW.png";
+}
+function MinivanClick(){
+    document.all.econom.src = "/cars/EconomCarBW.png";
+    document.all.business.src = "/cars/BusinessCarBW.png";
+    document.all.VIP.src = "/cars/VIPCarBW.png";
+    document.all.minivan.src = "/cars/MinivanCar.png";
+}
+function CourierClick(){
+    document.getElementById("courier").style.backgroundColor = '#416d85';
+}
+function BabySeatClick(){
+    document.getElementById("babySeat").style.background = '#416d85';
+}
+function ConditionerClick(){
+    document.getElementById("conditioner").style.background = '#416d85';
+}
+function EnglishClick(){
+    document.getElementById("english").style.background = '#416d85';
+}
+function NonSmokerClick(){
+    document.getElementById("nonSmoker").style.background = '#416d85';
+}
+function PetsClick(){
+    document.getElementById("pets").style.background = '#416d85';
+}
+function SilenceClick(){
+    document.getElementById("silecne").style.background = '#416d85';
+}
